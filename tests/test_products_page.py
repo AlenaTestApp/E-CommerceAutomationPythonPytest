b7c632ff6149931d2296a38944559c75b49e9aba
AlenaTestApp <elenakolocei@gmail.com>
2026-02-18 10:32:18 -0600

    Initial TC template for Products page

---
+++ tests/test_products_page.py
@@ -0,0 +1,19 @@
+"""Products page Test Cases"""
+
+
+def test_womens_category_shows_products(app):
+    # Navigate to Products Page, Select Women's Category:
+    # Observe the Relevant Products are displayed
+    pass
+
+
+def test_mens_category_shows_products(app):
+    # Navigate to Products Page, Select Men's Category:
+    # Observe the Relevant Products are displayed
+    pass
+
+
+def test_kids_category_shows_products(app):
+    # Navigate to Products Page, Select Kid's Category:
+    # Observe the Relevant Products are displayed
+    pass
