e3606330ca68e436d36ba48fb8f6aba510b19278
AlenaTestApp <elenakolocei@gmail.com>
2026-02-18 14:58:35 -0600

    Options for Headless Browser

--- utils/driver.py
+++ utils/driver.py
@@ -1,11 +1,25 @@
 """WebDriver setup and browser StartUp TearDown logic"""
 
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def create_driver():
     # driver creation and browser startup
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    return driver
+    options = Options()
 
+    # CI (Continuous Integration) environment on GitHub runner
+    if os.getenv("CI") == "true":
+        options.add_argument("--headless=new")           # headless
+        options.add_argument("--no-sandbox")             # no-sandbox  for CI
+        options.add_argument("--disable-dev-shm-usage")  # disable-dev-shm-usage, use /tmp
+        options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+
+    # maximize_window in Local environment
+    if os.getenv("CI") != "true":
+        driver.maximize_window()
+
+    return driver
